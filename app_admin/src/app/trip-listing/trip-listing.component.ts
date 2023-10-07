51012beed9b4878e3ce5392d9ed65e0b0d1856e9
Vincent 'Himitsu' Snow <vincentsnow@gmx.com>
2023-10-07 18:10:49 -0400

    Step 30 - created Trip Cards view and data service in admin app.

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+// import { trips } from '../data/trips';
+import { TripDataService } from '../services/trip-data.service';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
 
-  trips: Array<any> = trips;
+  //trips: Array<any> = trips;
+  trips: Trip[]; // Define 'trips' as an array of Trip objects
 
-  constructor() { }
+  message: string;
 
+  constructor(private tripDataService: TripDataService) { }
+
+  // Function to call the service's getTrips()
+  private getTrips(): void {
+    console.log('Inside TripListingComponent#getTrips');
+    this.message = 'Searching for trips';
+    // Store the returned Trips in a local class variable
+    this.tripDataService
+      .getTrips()
+        .then(foundTrips => {
+          this.message = foundTrips.length > 0 ? '' : 'No trips found';
+          this.trips = foundTrips;
+        });
+  }
+  // Invoke local getTrips function when class is initialized
   ngOnInit() {
+    this.getTrips();
   }
 
 }
