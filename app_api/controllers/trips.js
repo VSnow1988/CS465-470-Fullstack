67fc37e034986daa5572b0e08db450956e386b07
Vincent 'Himitsu' Snow <vincentsnow@gmx.com>
2023-10-07 19:33:29 -0400

    Step 43 - Finished wiring POST add trip route.

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -45,8 +45,36 @@ const tripsFindCode = async (req, res) => {
                 .json (trip);
            }
         });
-};
+}
+
+// POST: /trip/addTrip - add trip to the db
+const tripsAddTrip = async (req, res) => {
+    Model
+        .create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        },
+        (err, trip) => {
+            if(err) {
+                return res  
+                    .status(400) //bad request, invalid content
+                    .json(err);
+            } else {
+                return res
+                    .status(201) //created
+                    .json(trip);
+            }
+        });
+}
+
   module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip
   };
