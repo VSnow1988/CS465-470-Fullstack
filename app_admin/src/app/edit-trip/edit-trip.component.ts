7ab3fbde282b24fc90f7d1622f8260943a277fbe
Vincent 'Himitsu' Snow <vincentsnow@gmx.com>
2023-10-08 19:52:38 -0400

    Edit trip component finished. Cannot post data to db, edit-trip route not functional.

--- app_admin/src/app/edit-trip/edit-trip.component.ts
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Trip } from '../models/trip';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-edit-trip',
   templateUrl: './edit-trip.component.html',
   styleUrls: ['./edit-trip.component.css']
 })
+
 export class EditTripComponent implements OnInit {
 
-  constructor() { }
+  editForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private tripService: TripDataService
+  ) {  }
 
   ngOnInit() {
     // retrieve stashed tripId
@@ -16,6 +28,38 @@ export class EditTripComponent implements OnInit {
       alert("Something wrong, couldn't find where tripCode is stashed!");
       this.router.navigate(['']);
       return;
+    }
+  
+    // initialize form
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+    })
+    console.log('EditTripComponent#onInit calling TripDataService#getTrip(\'' + tripCode + '\')');
+    
+    this.tripService.getTrip(tripCode)
+      .then(data => {
+      console.log(data);
+      // Don't use editForm.setValue() as it will throw
+      this.editForm.patchValue(data[0]);
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.editForm.valid) {
+      this.tripService.updateTrip(this.editForm.value)
+      .then(data => {
+        console.log(data);
+        this.router.navigate(['']);
+    });
     }
   }
 
