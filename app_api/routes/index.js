75de15dc560186f7e0addc46663393b3b43794ea
Vincent 'Himitsu' Snow <vincentsnow@gmx.com>
2023-10-13 16:02:16 -0400

    Step 7 - Add new routes to the index api router for register and login.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const authController = require('../controllers/authentication');
 const tripsController = require('../controllers/trips');
 
+router
+    .route('/login')
+    .post(authController.login);
+router
+    .route('/register')
+    .post(authController.register);
 router
     .route('/trips')
     .get(tripsController.tripsList)
